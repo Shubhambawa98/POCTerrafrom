ef5e25ef131ef93c7896cfe01abff25610e2a07b
Shubham <shubhambawa98@gmail.com>
2023-04-26 09:46:00 +0530

    Update storage-account.tf

--- storage-account.tf
+++ storage-account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "shubham.dz.kumar"
   resource_group_name      = "github"
   location                 = "West Europe"
   account_tier             = "Standard"
